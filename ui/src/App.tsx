6d8677929ee10da1f40adee56bea4d6c5fc6d42b
Alfonso Catanzaro [MacBook] <alfonso.catanzaro@gmail.com>
2020-05-09 18:28:51 +0200

    ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className="content">
+      <h1>Hola cico!</h1>
+    </div>
+  );
+}
+
+export default App;
