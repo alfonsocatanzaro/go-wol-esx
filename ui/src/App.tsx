c877931936257e6d680100a44dc013f19757b755
Alfonso Catanzaro [MacBook] <alfonso.catanzaro@gmail.com>
2020-05-10 17:06:44 +0200

    UI Pages with router

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 // import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="content">
-      <h1>Hola cico!</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/main">Main</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/main">
+            <Main />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
