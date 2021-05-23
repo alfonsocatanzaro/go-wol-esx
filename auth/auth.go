6f247d9e2fc3f3b335c9d77bc4de98229a8ae035
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2021-05-23 18:54:03 +0200

    debug login

--- auth/auth.go
+++ auth/auth.go
@@ -7,7 +7,7 @@ import (
 	"path"
 	"time"
 
-	"github.com/auth0/go-jwt-middleware"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/tg123/go-htpasswd"
 )
@@ -17,20 +17,12 @@ var signKey = []byte("HSKJAHSKJ(/&JHBSDAKJHD")
 // LoginHandler Validate username and password and return jwt token
 func LoginHandler(dataPath string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		if !verbIsAllowed(w, r, http.MethodPost) {
 			return
 		}
 
 		var loginPayload LoginPayload
-		jsonDecoder := json.NewDecoder(r.Body)
-		err := jsonDecoder.Decode(&loginPayload)
-		if err != nil {
+		if err := loginPayload.decode(w, r); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
@@ -46,22 +38,67 @@ func LoginHandler(dataPath string) http.HandlerFunc {
 			return
 		}
 
-		token := jwt.New(jwt.SigningMethodHS256)
-		claims := token.Claims.(jwt.MapClaims)
-
-		claims["admin"] = true
-		claims["name"] = loginPayload.Username
-		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-
-		tokenString, err := token.SignedString(signKey)
+		err = writeAuthToken(w, loginPayload.Username)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(tokenString))
 	})
+}
+
+// LoginHandler debug method for validate useraname and password
+// username: admin
+// password: admin
+// and return jwt token
+func DebugLoginHandler(dataPath string) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !verbIsAllowed(w, r, http.MethodPost) {
+			return
+		}
+
+		var loginPayload LoginPayload
+		if err := loginPayload.decode(w, r); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if !(loginPayload.Username == "admin" && loginPayload.Password == "admin") {
+			http.Error(w, "Wrong username or passoword!", http.StatusUnauthorized)
+			return
+		}
+
+		err := writeAuthToken(w, loginPayload.Username)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	})
+}
+
+func writeAuthToken(w http.ResponseWriter, username string) error {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["admin"] = true
+	claims["name"] = username
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokenString, err := token.SignedString(signKey)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(tokenString))
+	return err
+}
+
+func verbIsAllowed(w http.ResponseWriter, r *http.Request, method string) bool {
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return false
+	}
+	if r.Method != method {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return false
+	}
+	return true
 }
 
 // JwtMiddleware for token validation
@@ -76,4 +113,10 @@ var JwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
 type LoginPayload struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+func (loginPayload *LoginPayload) decode(w http.ResponseWriter, r *http.Request) error {
+	jsonDecoder := json.NewDecoder(r.Body)
+	err := jsonDecoder.Decode(loginPayload)
+	return err
 }
