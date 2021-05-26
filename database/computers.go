101502242c0462f189893303ab5127228dd31798
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2021-05-26 23:33:05 +0200

    BoltDB

--- database/computers.go
+++ database/computers.go
@@ -1,1 +1,68 @@
 package database
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/alfonsocatanzaro/go-wol-esx/models"
+	"github.com/boltdb/bolt"
+)
+
+const COMPUTERS = "Computers"
+
+
+var dbFilePath string
+
+func InitDb(dbFile string) error {
+	dbFilePath = dbFile
+	db, err := bolt.Open(dbFilePath, 0600, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err = tx.CreateBucketIfNotExists([]byte(COMPUTERS))
+		return err
+	})
+	return nil
+}
+
+func Save(c *models.Computer) error {
+	db, err := bolt.Open(dbFilePath, 0600, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	id := []byte(fmt.Sprint(c.ID))
+	data, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(COMPUTERS))
+		err := b.Put(id, data)
+		return err
+	})
+	return err
+}
+
+func GetAll() ([]models.Computer, error) {
+	db, err := bolt.Open(dbFilePath, 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	computers := make([]models.Computer, 0)
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(COMPUTERS))
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			comp := models.Computer{}
+			json.Unmarshal(v, &comp)
+			computers = append(computers, comp)
+		}
+		return nil
+	})
+	return computers, nil
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/auth0/go-jwt-middleware v0.0.0-20200507191422-d30d7b9ece63
+	github.com/boltdb/bolt v1.3.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/handlers v1.4.2
 	github.com/tg123/go-htpasswd v1.0.0
